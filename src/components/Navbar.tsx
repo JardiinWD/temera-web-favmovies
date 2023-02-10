982f40ac0c119e4203c8ff8ee0d61e97a6a282e3
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2023-02-10 17:50:54 +0100

    #16 - Aggiunta navbar dinamica. Ora è presente anche per le versioni smartphone.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Heading from './UI/Heading'
 import { Container, Row } from 'reactstrap'
 import './styles/Navbar.scss'
 import { Link } from 'react-router-dom'
+import {FaBars, FaTimes} from 'react-icons/fa'
 import React, {Fragment, useState, useEffect} from 'react'
 import type { FunctionComponent } from 'react'
 import { useLocation } from 'react-router-dom'
@@ -10,6 +11,8 @@ const Navbar: FunctionComponent = () => {
   
     // Variabile di stato per la pagina corrente
   const [isCurrentPage, setIsCurrentPage] = useState<string>('')
+  // Variabile di stato per la nav responsive
+  const [navToggle, setNavToggle] = useState<boolean>(false)
   // utilizzo la location e la salvo in una variabile
   const pageLocation = useLocation()
   
@@ -18,6 +21,26 @@ const Navbar: FunctionComponent = () => {
     setIsCurrentPage(pageLocation.pathname === '/favorites' ? 'Favorites' : 'Top Rated')
   }, [pageLocation.pathname])
 
+  // useEffect che mi aiuta a sistemare il comportamento dell resize sulla Navbar
+  useEffect(() => {
+    // Handler per aggiornare variabile di stato navToggle al resize
+    const handleViewportResize = () => {
+      if (window.innerWidth >= 768 && navToggle) setNavToggle(false)
+    }
+    // Aggiungo evento listener alla window al resize
+    window.addEventListener("resize", handleViewportResize)
+
+    // Restituisco comunque una function che rimuove addEventListener
+    return () => {
+      window.removeEventListener("resize", handleViewportResize)
+    }
+  }, [navToggle])
+
+  // Handler della nav dinamica
+  const handlerNavToggle = () => {
+    setNavToggle(!navToggle)
+  }
+
   return (
     <Fragment>
       <Container className='nav-container'>
@@ -25,7 +48,7 @@ const Navbar: FunctionComponent = () => {
           <Link to='/'>
             <Heading type="logo-title" title="Movies" />
           </Link>
-          {/* TODO: Da wrappare in una sidebar laterale a scomparsa in "sm" */}
+          {/* Oltre 768px */}
           <div className='nav-links'>
             <Link to='/'>
               <span className={`${isCurrentPage === 'Top Rated' ? 'active-page' : ''}`}>Top Rated</span>
@@ -34,6 +57,28 @@ const Navbar: FunctionComponent = () => {
               <span className={`${isCurrentPage === 'Favorites' ? 'active-page' : ''}`}>Favorites</span>
             </Link>
           </div>
+          {/* Toggle per il responsive */}
+          <div className='navbar-responsive_toggle'>
+            {
+              !navToggle ? (
+                <FaBars onClick={handlerNavToggle} />
+              ) : (
+                <FaTimes onClick={handlerNavToggle} />
+              )
+            }
+          </div>
+          {
+            navToggle && (
+              <div className='navbar-responsive_wrapper'>
+                  <Link onClick={handlerNavToggle} to='/'>
+                    <span className={`${isCurrentPage === 'Top Rated' ? 'active-page' : ''}`}>Top Rated</span>
+                  </Link>
+                  <Link onClick={handlerNavToggle} to='/favorites'>
+                    <span className={`${isCurrentPage === 'Favorites' ? 'active-page' : ''}`}>Favorites</span>
+                  </Link>
+              </div>
+            )
+          }
         </Row>
       </Container>
     </Fragment>
